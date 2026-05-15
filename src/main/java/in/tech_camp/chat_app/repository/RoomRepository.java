d3466090b8b4c4896bb42f07416eb4556f9b6482
kaneryo0723 <r.kanezuka@infomart.co.jp>
2026-05-15 16:13:17 +0900

    チャット画面の編集

--- src/main/java/in/tech_camp/chat_app/repository/RoomRepository.java
+++ src/main/java/in/tech_camp/chat_app/repository/RoomRepository.java
@@ -3,6 +3,7 @@ package in.tech_camp.chat_app.repository;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
 
 import in.tech_camp.chat_app.entity.RoomEntity;
 
@@ -12,4 +13,9 @@ public interface RoomRepository {
   @Insert("INSERT INTO rooms(name) VALUES(#{name})")
   @Options(useGeneratedKeys=true,keyProperty="id")
   void insert(RoomEntity roomEntity);
+
+  // RoomUserRepositoryで使う用のメソッド
+  @Select("SELECT * FROM rooms WHERE id = #{id}")
+  RoomEntity findById(Integer id);
+  
 }
