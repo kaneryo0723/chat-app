16dd30dc70de074f643cbfdf69765cfc83c2bcbf
kaneryo0723 <r.kanezuka@infomart.co.jp>
2026-05-15 09:27:21 +0900

    新規作成画面の表示

---
+++ src/main/java/in/tech_camp/chat_app/controller/RoomController.java
@@ -0,0 +1,19 @@
+package in.tech_camp.chat_app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import in.tech_camp.chat_app.form.RoomForm;
+
+
+@Controller
+public class RoomController {
+  @GetMapping("/rooms/new")
+  //チャット作成画面の表示
+  public String showRoomNew(Model model) {
+    model.addAttribute("roomForm",new RoomForm());
+      return "rooms/new";
+  }
+  
+}
